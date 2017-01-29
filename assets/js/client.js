e001733c2c9cefd2ccb68524da9fb0bf57cf0bb0
ipri <i.priladnyh@gmail.com>
2017-01-29 07:48:33 +0300

    Оптимизация изображений и шрифтов:
    1. Сократил размер изображений путем допустимого уменьшения качества и
       обрезки неиспользуемых частей изображений;
    2. Объединил все изображения иконок в один файл - icons.png. Внес соответсвующие
       изменения в файл стилей main.css
    3. Существенно сократил размер подгружаемых шрифтов.
    
    Дополнительно:
    
    1. Минифицировал файл стилей bootstrap.css -> bootstrap.min.css;
    2. Пересобрал bootstrap-bundle.js из-за ошибок.

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,9 +1,10 @@
 let mainItem = document.getElementById("main");
-let mainItemIcon = mainItem.getElementsByTagName('img')[0];
+let banner1 = document.getElementById("banner-1");
+let bannerSrc = `<a target=_blank href="http://ya.ru">
+                    <img src="assets/img/banner.png" border=0>
+                </a>`;
 
-mainItem.className = 'active';
-mainItemIcon.src = mainItemIcon.src.replace('.png', '-w.png');
-
-
-
-
+let requestId = requestAnimationFrame(()=>{
+    mainItem.className = 'active';
+    banner1.insertAdjacentHTML(`afterBegin`, bannerSrc);
+});
